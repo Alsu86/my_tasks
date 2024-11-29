274eb102ca95eeaf6355ab1c27c9afdfdf2f2629
Alsu86 <166997937+Alsu86@users.noreply.github.com>
2024-11-29 16:48:59 +0300

    Update module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -13,27 +13,23 @@ class Bank:
             with self.lock:
                 self.balance += amount
                 print(f"Пополнение: {amount}. Баланс: {self.balance}")
-                if self.balance >= 500 and self.lock.locked():
-                    self.lock.release()
             time.sleep(0.001)
 
     def take(self):
         for _ in range(100):
             amount = random.randint(50, 500)
-            print(f"Запрос на {amount}")
             with self.lock:
                 if amount <= self.balance:
                     self.balance -= amount
                     print(f"Снятие: {amount}. Баланс: {self.balance}")
                 else:
                     print("Запрос отклонён, недостаточно средств")
-                    self.lock.acquire()
             time.sleep(0.001)
 
 bk = Bank()
 
-th1 = threading.Thread(target=Bank.deposit, args=(bk,))
-th2 = threading.Thread(target=Bank.take, args=(bk,))
+th1 = threading.Thread(target=bk.deposit)
+th2 = threading.Thread(target=bk.take)
 
 th1.start()
 th2.start()
